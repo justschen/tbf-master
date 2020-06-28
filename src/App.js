381e6e274a8dd1b9d14020832cde28d7cfe1f73b
Samarth Goel <sgoel9@berkeley.edu>
2020-06-27 23:16:15 -0500

    init commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './css/App.css';
+
+import Landing from './components/Landing';
+
+const App = () => {
+	return (
+		<div className="App">
+			<Landing />
+		</div>
+	);
+};
+
+export default App;
