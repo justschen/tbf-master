bd6095b5831faa96bb19195132b2dbf60584054c
Aditya Bhawal <aditya.bhawal@gmail.com>
2020-07-04 23:21:02 -0700

    Default Styling + Tinkering
    In "App.css", added default css styles for headers, paragraphs, and buttons.
    Added 4 buttons in about page.
    Changed header to display inline-block and removed top margin. Added border radii.

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -19,7 +19,10 @@ const About = () => {
             
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam sunt id inventore ipsam ea iusto esse sint voluptates iure blanditiis, dignissimos, dicta, autem consequuntur! Nemo accusamus facere reiciendis obcaecati! Tempore!</p>
             <div class="form">
-				<h2>Stuff</h2>
+                <button>Meet Our Team</button>
+                <button>Advisory Committee</button>
+                <button>Our Committees</button>
+                <button>Legacy</button>
             </div>
         </section>
     </div>
