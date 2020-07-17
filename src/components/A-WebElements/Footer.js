8ae77b68fca711b348df80ec06d544f2377ff6ac
Aditya Bhawal <aditya.bhawal@gmail.com>
2020-07-17 16:26:11 -0700

    fix the fucky wucky in the footer uwu

--- src/components/A-WebElements/Footer.js
+++ src/components/A-WebElements/Footer.js
@@ -15,39 +15,49 @@ const Footer = (props) => {
                         <br/>info@forum.berkeley.edu
                 </p>
                 <div className="footer-links">
-                        <Link to="/" className="link-tag">
-                                <p className="footer-link-text">Home</p>
-                        </Link>
-                        <Link to="/events" className="link-tag">
-                                <p className="footer-link-text">Events</p>
-                        </Link>
-                        <Link to="/team" className="link-tag">
-                                <p className="footer-link-text">Meet Our Team</p>
-                        </Link>
-                        <Link to="/contact" className="link-tag">
-                                <p className="footer-link-text">Contact Us</p>
-                        </Link>
-                        <Link to="/about" className="link-tag">
-                                <p className="footer-link-text">About Us</p>
-                        </Link>
-                        <Link to="/calendar" className="link-tag">
-                                <p className="footer-link-text">Calendar</p>
-                        </Link>
-                        <Link to="" className="link-tag">
-                                <p className="footer-link-text">Advisory Committee</p>
-                        </Link>
-                        <Link to="" className="link-tag">
-                                <p className="footer-link-text">Privacy Policy</p>
-                        </Link>
-                        <Link to="/convoBlocks" className="link-tag">
-                                <p className="footer-link-text">Conversations</p>
-                        </Link>
-                        <Link to="/pastEvents" className="link-tag">
-                                <p className="footer-link-text">Past Events</p>
-                        </Link>
-                        <Link to="/join" className="link-tag">
-                                <p className="footer-link-text">Join Us</p>
-                        </Link>
+                        <div className="footer-col">
+                                <Link to="/" className="link-tag">
+                                        <p className="footer-link-text">Home</p>
+                                </Link>
+                                <Link to="/events" className="link-tag">
+                                        <p className="footer-link-text">Events</p>
+                                </Link>
+                                <Link to="/team" className="link-tag">
+                                        <p className="footer-link-text">Meet Our Team</p>
+                                </Link>
+                        </div>
+                        
+                        <div className="footer-col">
+                                <Link to="/contact" className="link-tag">
+                                        <p className="footer-link-text">Contact Us</p>
+                                </Link>
+                                <Link to="/about" className="link-tag">
+                                        <p className="footer-link-text">About Us</p>
+                                </Link>
+                                <Link to="/calendar" className="link-tag">
+                                        <p className="footer-link-text">Calendar</p>
+                                </Link>
+                        </div>
+                        <div className="footer-col">
+                                <Link to="" className="link-tag">
+                                        <p className="footer-link-text">Advisory Committee</p>
+                                </Link>
+                                <Link to="" className="link-tag">
+                                        <p className="footer-link-text">Privacy Policy</p>
+                                </Link>
+                                <Link to="/convoBlocks" className="link-tag">
+                                        <p className="footer-link-text">Conversations</p>
+                                </Link>
+                        </div>
+                        
+                        <div className="footer-col">
+                                <Link to="/pastEvents" className="link-tag">
+                                        <p className="footer-link-text">Past Events</p>
+                                </Link>
+                                <Link to="/join" className="link-tag">
+                                        <p className="footer-link-text">Join Us</p>
+                                </Link>
+                        </div>
                 </div>
                 <img src={wdbLogoPath} alt='wdb-logo' className='wdb-logo'/>
                         
