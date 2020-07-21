e2a3c4b7a326fd141bba7fd846a624589a6f220c
Samarth Goel <samarthgoel@Samarths-Air.hsd1.il.comcast.net>
2020-07-21 17:00:07 -0500

    fixed footer formatting

--- src/components/A-WebElements/Footer.js
+++ src/components/A-WebElements/Footer.js
@@ -1,67 +1,84 @@
 import React from 'react';
 import { Link } from '@reach/router';
 
-import '../../css/Footer.css'
+import '../../css/Footer.scss';
 import tbfLogoPath from '../../img/footerlogo.png';
 import wdbLogoPath from '../../img/wdblogo.png';
 
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 const Footer = (props) => {
 	return (
-        <div className="footer">
-                <img src={tbfLogoPath} alt='tbf-logo' className='tbf-logo'/>
-                <p className='contact-info'>The Berkeley Forum at the University of California
-                        <br/><br/>432 Eshleman Hall, Berkeley, CA 94720-4520
-                        <br/><br/>General Inquiries
-                        <br/>info@forum.berkeley.edu
-                </p>
-                <div className="footer-links">
-                        <div className="footer-col">
-                                <Link to="/" className="link-tag">
-                                        <p className="footer-link-text">Home</p>
-                                </Link>
-                                <Link to="/events" className="link-tag">
-                                        <p className="footer-link-text">Events</p>
-                                </Link>
-                                <Link to="/team" className="link-tag">
-                                        <p className="footer-link-text">Meet Our Team</p>
-                                </Link>
-                        </div>
-                        
-                        <div className="footer-col">
-                                <Link to="/contact" className="link-tag">
-                                        <p className="footer-link-text">Contact Us</p>
-                                </Link>
-                                <Link to="/about" className="link-tag">
-                                        <p className="footer-link-text">About Us</p>
-                                </Link>
-                                <Link to="/calendar" className="link-tag">
-                                        <p className="footer-link-text">Calendar</p>
-                                </Link>
-                        </div>
-                        <div className="footer-col">
-                                <Link to="" className="link-tag">
-                                        <p className="footer-link-text">Advisory Committee</p>
-                                </Link>
-                                <Link to="" className="link-tag">
-                                        <p className="footer-link-text">Privacy Policy</p>
-                                </Link>
-                                <Link to="/convoBlocks" className="link-tag">
-                                        <p className="footer-link-text">Conversations</p>
-                                </Link>
-                        </div>
-                        
-                        <div className="footer-col">
-                                <Link to="/pastEvents" className="link-tag">
-                                        <p className="footer-link-text">Past Events</p>
-                                </Link>
-                                <Link to="/join" className="link-tag">
-                                        <p className="footer-link-text">Join Us</p>
-                                </Link>
-                        </div>
-                </div>
-                <img src={wdbLogoPath} alt='wdb-logo' className='wdb-logo'/>
-                        
-        </div>
+		<Container fluid className="footer">
+			<Col
+				xs={5}
+				className="left-col"
+				style={{ borderRight: '2px solid #FFFFFF' }}
+			>
+				<img src={tbfLogoPath} alt="tbf-logo" className="tbf-logo" />
+				<div className="contact-info">
+					<p>The Berkeley Forum at the University of California</p>
+					<p>432 Eshleman Hall, Berkeley, CA 94720-4520</p>
+					<p>
+						General Inquiries
+						<br />
+						info@forum.berkeley.edu
+					</p>
+				</div>
+			</Col>
+			<Col xs={7} className="right-col">
+				<Row className="footer-links">
+					<Col xs={3} className="footer-col">
+						<Link to="/" className="link-tag">
+							<p>Home</p>
+						</Link>
+						<Link to="/events" className="link-tag">
+							<p>Events</p>
+						</Link>
+						<Link to="/team" className="link-tag">
+							<p>Meet Our Team</p>
+						</Link>
+					</Col>
+
+					<Col xs={3} className="footer-col">
+						<Link to="/contact" className="link-tag">
+							<p>Contact Us</p>
+						</Link>
+						<Link to="/about" className="link-tag">
+							<p>About Us</p>
+						</Link>
+						<Link to="/calendar" className="link-tag">
+							<p>Calendar</p>
+						</Link>
+					</Col>
+					<Col xs={3} className="footer-col">
+						<Link to="" className="link-tag">
+							<p>Advisory Committee</p>
+						</Link>
+						<Link to="" className="link-tag">
+							<p>Privacy Policy</p>
+						</Link>
+						<Link to="/convoBlocks" className="link-tag">
+							<p>Conversations</p>
+						</Link>
+					</Col>
+
+					<Col xs={3} className="footer-col">
+						<Link to="/pastEvents" className="link-tag">
+							<p>Past Events</p>
+						</Link>
+						<Link to="/join" className="link-tag">
+							<p>Join Us</p>
+						</Link>
+					</Col>
+				</Row>
+				<Row className="justify-content-end">
+					<img src={wdbLogoPath} alt="wdb-logo" className="wdb-logo" />
+				</Row>
+			</Col>
+		</Container>
 	);
 };
 
--- src/components/About/About.js
+++ src/components/About/About.js
@@ -2,43 +2,49 @@ import React from 'react';
 import { Link } from '@reach/router';
 
 import '../../css/About.css';
-import bigPic from '../../img/aboutUsJPG.JPG';
+import AboutHeader from '../../img/aboutUsJPG.JPG';
 import Header from '../A-WebElements/Header';
 import Footer from '../A-WebElements/Footer';
 
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 const About = () => {
 	return (
-    <div class="about"> 
-        <section class="top">
-        <Header name="About Us" image={bigPic}/>
-        
-        <h1 class='aboutPic'></h1>
-        
-        </section>
-        <section class='bottom'>
-            
-            <p>
-                “The Berkeley Forum is a non-partisan forum for the expression and debate of a wide range of ideas. 
-                Founded in 2012, the Forum organizes panels, debates, and talks by leading experts and distinguished 
-                persons on issues both topical and timeless. Forum events are open to the public and broadcasted online 
-                on our Facebook Page. Admission to Forum events is free for UC Berkeley students, staff, and faculty. 
-                An admission fee may be required for other event attendees, which will be collected at the event.
-                <br/><br/>
-                The Forum is run entirely by undergraduate students at the University of California, Berkeley 
-                from a diverse range of academic and professional backgrounds. The Forum provides students valuable 
-                first-hand experience and a channel to explore and challenge the ideas that motivate them.”
-                </p>
-            <div class="form">
-                <button>Meet Our Team</button>
-                <button>Advisory Committee</button>
-                <button>Our Committees</button>
-                <button>Legacy</button>
-            </div>
-        </section>
-        <Footer/>
-    </div>
-		
-
+		<div className="about">
+			<Header name="About Us" image={AboutHeader} />
+			<Container fluid className="about-content">
+				<Row>
+					<p>
+						“The Berkeley Forum is a non-partisan forum for the expression and
+						debate of a wide range of ideas. Founded in 2012, the Forum
+						organizes panels, debates, and talks by leading experts and
+						distinguished persons on issues both topical and timeless. Forum
+						events are open to the public and broadcasted online on our Facebook
+						Page. Admission to Forum events is free for UC Berkeley students,
+						staff, and faculty. An admission fee may be required for other event
+						attendees, which will be collected at the event.
+					</p>
+				</Row>
+				<Row className="middle-row">
+					<p>
+						The Forum is run entirely by undergraduate students at the
+						University of California, Berkeley from a diverse range of academic
+						and professional backgrounds. The Forum provides students valuable
+						first-hand experience and a channel to explore and challenge the
+						ideas that motivate them.”
+					</p>
+				</Row>
+				<Row className="bottom-navbar">
+					<p>Meet Our Team</p>
+					<p>Advisory Committee</p>
+					<p>Our Committees</p>
+					<p>Legacy</p>
+				</Row>
+			</Container>
+			<Footer />
+		</div>
 	);
 };
 
