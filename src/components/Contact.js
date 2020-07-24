bddc0d6dc09143b5e9b2ebb3841e1456a2ebd05d
Justin Chen <justinshawnchen@berkeley.edu>
2020-07-24 11:17:17 -0700

    Contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,13 +1,41 @@
 import React from 'react';
 import { Link } from '@reach/router';
 
-import Button from 'react-bootstrap/Button';
-
+import '../css/Contact.css';
+import Header from './A-WebElements/Header';
+import Footer from './A-WebElements/Footer';
+import contactHeader from '../img/contactPic.png';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 const Contact = () => {
 	return (
-		<div className="contact">
-			<h1>Contact Us</h1>
+		<div className="decalPage">
+			<Header name="Contact Us" image={contactHeader} />
+			
+			<Container fluid className="decal">
+					
+				<Row className="top-row">
+					<p>
+					For general inquiries, please use the submission box below. We’ll make sure to answer your question as soon as we can!
+					</p>
+					
+				</Row>
+				
+				<Row className="middle-row">
+					<p>
+						
+						
+					</p>
+				</Row>
+				<Row className="bottom-row">
+					<p>
+						
+						</p>
+				</Row>
+			</Container>
+			<Footer />
 		</div>
 	);
 };
