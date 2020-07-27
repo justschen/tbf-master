d98fda2b760fd73cc180d69c3478c99e900c1079
Aditya Bhawal <aditya.bhawal@gmail.com>
2020-07-27 00:40:24 -0700

    Playing with Bootstrap Carousel
    Imported Carousel, used it in the landing page and made a very very rough implementation to style/play with

--- src/components/A-WebElements/Header.js
+++ src/components/A-WebElements/Header.js
@@ -5,81 +5,112 @@ import '../../css/Header.css'
 
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/NavBar';
+import Carousel from 'react-bootstrap/Carousel';
 
 import tasslePath from '../../img/social-tassle.png';
 
+
+
+import AboutHeader from '../../img/aboutUsJPG.JPG';
 const Header = (props) => {
     let middle;
     const bg = {
     backgroundImage:`url(${props.image})`,
         background: props.image,
     };
-    
-	return (
-        <div className="stuff" style={bg}>
-		    <div className="header">
-			<Link to="/" className="landing-link">
-                <h1>The Berkeley Forum</h1>
-            </Link>
-            <img src={tasslePath} alt='tassle-socials' className='socials-tassle'/>
-            <div className='bar'>
-            <Navbar collapseOnSelect expand="lg" >
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="mm">
-                    <div className="dropdown">
-                
-                                <Link to="/about" className="link-tag">
-                                    <p className="nav-text">About Us
-                                    <span class="glyphicon glyphicon-on logo-small"></span>
-                                    <span class="icon-bar"></span>
-                                    </p>
-                                </Link>
-                                <div className="dropdown-content-about">
-                                    <Link to="/team" className="link-tag">
-                                        <p className="nav-text">Meet the<br/>Team</p>
-                                    </Link>
-                                </div>
-                    </div>
-                    <div className="dropdown">
-                                <Link to="/calendar" className="link-tag">
-                                    <p className="nav-text">Events</p>
-                                </Link>
-                                <div className="dropdown-content-events">
-                                    <Link to="/pastEvents" className="link-tag">
-                                        <p className="nav-text">Past<br/>Events</p>
-                                    </Link>
-                                    <Link to="/posters" className="link-tag">
-                                        <p className="nav-text">Speaker<br/>Posters</p>
-                                    </Link>
-                                </div>
-                    </div>
-                    <Link to="/join" class='link-tag'>
-                        <p className="nav-text">Join Us</p>
-                    </Link>
-                    <Link to="/convoBlocks" class='link-tag'>
-                        <p className="nav-text">Conversations</p>
-                        </Link>
-                    <Link to="/contact" class='link-tag'>
-                        <p className="nav-text">Contact</p>
-                    </Link>
-                    <Link to="/decal" class='link-tag'>
-                        <p className="nav-text">Decal</p>
-                    </Link> 
+    if(props.name.localeCompare("landing") == 0) {
+        return (
+            <Carousel>
+                <Carousel.Item>
+                    <img
+                    className="d-block w-100"
+                    src={props.image}
+                    alt="First slide"
+                    />
+                    <Carousel.Caption>
+                    <h3>Fall 2020 Lineup</h3>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                    className="d-block w-100"
+                    src={AboutHeader}
+                    alt="Third slide"
+                    />
+
+                    <Carousel.Caption>
+                    <h3>Keep Up with the Forum</h3>
+                    </Carousel.Caption>
+                </Carousel.Item>
+            </Carousel>
+        );
+    } else {
+        return (
+            <div className="stuff" style={bg}>
+                <div className="header">
+                <Link to="/" className="landing-link">
+                    <h1>The Berkeley Forum</h1>
+                </Link>
+                <img src={tasslePath} alt='tassle-socials' className='socials-tassle'/>
+                <div className='bar'>
+                <Navbar collapseOnSelect expand="lg" >
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="mm">
+                        <div className="dropdown">
                     
-                </Nav>
-    
-                </Navbar.Collapse>
-            </Navbar>
-            
+                                    <Link to="/about" className="link-tag">
+                                        <p className="nav-text">About Us
+                                        <span class="glyphicon glyphicon-on logo-small"></span>
+                                        <span class="icon-bar"></span>
+                                        </p>
+                                    </Link>
+                                    <div className="dropdown-content-about">
+                                        <Link to="/team" className="link-tag">
+                                            <p className="nav-text">Meet the<br/>Team</p>
+                                        </Link>
+                                    </div>
+                        </div>
+                        <div className="dropdown">
+                                    <Link to="/calendar" className="link-tag">
+                                        <p className="nav-text">Events</p>
+                                    </Link>
+                                    <div className="dropdown-content-events">
+                                        <Link to="/pastEvents" className="link-tag">
+                                            <p className="nav-text">Past<br/>Events</p>
+                                        </Link>
+                                        <Link to="/posters" className="link-tag">
+                                            <p className="nav-text">Speaker<br/>Posters</p>
+                                        </Link>
+                                    </div>
+                        </div>
+                        <Link to="/join" class='link-tag'>
+                            <p className="nav-text">Join Us</p>
+                        </Link>
+                        <Link to="/convoBlocks" class='link-tag'>
+                            <p className="nav-text">Conversations</p>
+                            </Link>
+                        <Link to="/contact" class='link-tag'>
+                            <p className="nav-text">Contact</p>
+                        </Link>
+                        <Link to="/decal" class='link-tag'>
+                            <p className="nav-text">Decal</p>
+                        </Link> 
+                        
+                    </Nav>
+        
+                    </Navbar.Collapse>
+                </Navbar>
+                
+                </div>
             </div>
-		</div>
 
 
-        <h2 class="text">{props.name}</h2>
-     
-    </div>
-	);
+            <h2 class="text">{props.name}</h2>
+        
+        </div>
+        );
+    }
 };
 
 export default Header;
