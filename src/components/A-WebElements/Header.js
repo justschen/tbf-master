6787bf6d70343e228ddf4426fa168a55ffc09ca9
Justin Chen <justinshawnchen@berkeley.edu>
2020-07-29 03:14:40 -0700

    Conversations

--- src/components/A-WebElements/Header.js
+++ src/components/A-WebElements/Header.js
@@ -18,7 +18,7 @@ import HeaderBar from './HeaderBar';
 const Header = (props) => {
     let middle;
     const bg = {
-    backgroundImage:`url(${props.image})`,
+        backgroundImage:`url(${props.image})`,
         background: props.image,
     };
     if(props.name.localeCompare("landing") == 0) {
@@ -44,7 +44,6 @@ const Header = (props) => {
                         src={landingMailPhoto}
                         alt="Third slide"
                         />
-
                         <Carousel.Caption className = "d-flex flex-column h-50 align-items-left justify-content-start">
                             <h3>Keep Up with the Forum</h3>
                             <p>Subscribe to Our Mailing List</p>
@@ -53,6 +52,71 @@ const Header = (props) => {
                 </Carousel>
             </div>
         );
+    } else if(props.name.localeCompare("randPaul") == 0) {
+        return (
+            <div className="space">
+                <div className="header">
+                <Link to="/" className="landing-link">
+                    <h1>The Berkeley Forum</h1>
+                </Link>
+                <img src={tasslePath} alt='tassle-socials' className='socials-tassle'/>
+                <div className='bar'>
+                <Navbar collapseOnSelect expand="lg" >
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="mm">
+                        <div className="dropdown">
+                                    <Link to="/about" className="link-tag">
+                                        <p className="nav-text">About Us
+                                        <span class="glyphicon glyphicon-chevron-down logo-small"></span>
+                                        <span class="icon-bar"></span>
+                                        </p>
+                                    </Link>
+                                    <div className="dropdown-content-about">
+                                        <Link to="/team" className="link-tag">
+                                            <p className="nav-text">Meet the<br/>Team</p>
+                                        </Link>
+                                    </div>
+                        </div>
+                        <div className="dropdown">
+                                    <Link to="/calendar" className="link-tag">
+                                        <p className="nav-text">Events</p>
+                                    </Link>
+                                    <div className="dropdown-content-events">
+                                        <Link to="/pastEvents" className="link-tag">
+                                            <p className="nav-text">Past<br/>Events</p>
+                                        </Link>
+                                        <Link to="/posters" className="link-tag">
+                                            <p className="nav-text">Speaker<br/>Posters</p>
+                                        </Link>
+                                    </div>
+                        </div>
+                        <Link to="/join" class='link-tag'>
+                            <p className="nav-text">Join Us</p>
+                        </Link>
+                        <Link to="/convoBlocks" class='link-tag'>
+                            <p className="nav-text">Conversations</p>
+                            </Link>
+                        <Link to="/contact" class='link-tag'>
+                            <p className="nav-text">Contact</p>
+                        </Link>
+                        <Link to="/decal" class='link-tag'>
+                            <p className="nav-text">Decal</p>
+                        </Link> 
+                        
+                    </Nav>
+        
+                    </Navbar.Collapse>
+                </Navbar>
+                
+                </div>
+            </div>
+
+        
+        </div>
+       
+        );
+    
     } else {
         return (
             <div className="stuff" style={bg}>
