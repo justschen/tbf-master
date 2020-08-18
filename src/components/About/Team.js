914d7a281e9a7771c2c567691af774c49be553ff
Aditya Bhawal <aditya.bhawal@gmail.com>
2020-08-17 22:15:37 -0700

    Near complete Team page
    Added higher-res portraints, adjusted styling, added a database file for all the members and filled them all out

--- src/components/About/Team.js
+++ src/components/About/Team.js
@@ -12,57 +12,7 @@ import Header from '../A-WebElements/Header';
 import Footer from '../A-WebElements/Footer';
 import TeamCard from '../A-WebElements/TeamCard';
 
-import CindyTang from '../../img/bios/Cindy_Tang.png';
-import AparnaIyer from '../../img/bios/Aparna_Iyer.png';
-import EthanQi from '../../img/bios/Ethan_Qi.png';
-import AbhayAggarwal from '../../img/bios/Abhay_Aggarwal.png';
-import DavidLee from '../../img/bios/David_Lee.png';
-import KahoOtake from '../../img/bios/Kaho_Otake.png';
-import CJHines from '../../img/bios/CJ_Hines.png';
-
-import DanielVoskoboynik from '../../img/bios/Daniel_Voskoboynik.png';
-import AnkitaInamdar from '../../img/bios/Ankita_Inamdar.png';
-import NayanChavan from '../../img/bios/Nayan_Chavan.png';
-
-var execData = [
-	[
-		'Cindy Tang',
-		'President',
-		'Cindy Tang is a third-year student studying Business Administration ' +
-			'and Sociology with a minor in Human Rights Interdisciplinary.' +
-			'Originally from Columbus, OH, Cindy has passions for inclusion' +
-			'and diversity, people, and education. In Cindy’s free time, she' +
-			'enjoys hiking, vintage shopping, and trying new restaurants.' +
-			'Cindy is currently an RA in Blackwell Hall and is a huge fan of' +
-			'the Ohio State Buckeyes.',
-		CindyTang,
-	],
-	[
-		'Aparna Iyer',
-		'VP of Events',
-		'Aparna is a third- year studying Political Science and Human Rights. ' +
-			'She joined the Forum in her freshman year and has loved the process of ' +
-			'inviting speakers, moderating talks with fascinating people, and learning ' +
-			'more about how to host events. Her favorite events thus far have been with ' +
-			'SNL Director Don Roy King and writer Gene Luen Yang. As VP of Programming, ' +
-			'she facilitates speaker invitations that will determine the lineup for Fall ' +
-			'2020. She enjoys politics, dance, and stand-up comedy.',
-		AparnaIyer,
-	],
-	['Ethan Qi', 'VP of Finance', '', EthanQi],
-	['Abhay Aggarwal', 'VP of Moderating', '', AbhayAggarwal],
-	['David Lee', 'VP of Programming', '', DavidLee],
-	['Kaho Otake', 'VP of Communication', '', KahoOtake],
-	['CJ Hines', 'VP of Systems & Technology', '', CJHines],
-];
-
-var directorData = [
-	['Nayan Chavan', 'Director of Social Affairs', '', NayanChavan],
-	['Ankita Inamdar', 'Director of Development', '', AnkitaInamdar],
-	['Daniel Voskoboynik', 'Director of Membership', '', DanielVoskoboynik],
-];
-
-var memberData = [];
+import { execData, directorData, memberData } from './TeamData.js';
 
 const Team = () => {
 	return (
@@ -101,6 +51,30 @@ const Team = () => {
 				</Row>
 				<Row>
 					<h1 className="section-divider">Members</h1>
+				</Row>
+				<Row>
+					{memberData.map((member) => (
+						<Col xs={4}>
+							<TeamCard
+								bio={member[2]}
+								name={member[0]}
+								role={member[1]}
+								image={member[3]}
+							/>
+							<p className="member-label">Committee Member</p>
+						</Col>
+					))}
+				</Row>
+				<Row className="bottom-navbar">
+					<Link to="/advisory">
+						<p className="link-text">Advisory Committee</p>
+					</Link>
+					<Link to="/committees">
+						<p className="link-text">Our Committees</p>
+					</Link>
+					<Link to="/legacy">
+						<p className="link-text">Legacy</p>
+					</Link>
 				</Row>
 			</Container>
 			<Footer />
