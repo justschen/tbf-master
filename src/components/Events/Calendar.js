b3fa9bfa98beb18baca1e08cc76d1253d57922a3
Justin Chen <justinshawnchen@berkeley.edu>
2020-08-19 05:44:04 -0700

    Fix

--- src/components/Events/Calendar.js
+++ src/components/Events/Calendar.js
@@ -15,84 +15,30 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
+import { currentEventData } from './CurrentEventData.js';
+import EventCard from './EventCard.js';
+
 const PastEvents = () => {
 	return (
 		<div className="past">
 			<Header name="calendar" image={PosterHeader} />
-			<h4>Upcoming Events</h4>
-			<section className="block">
-				<Container className="personBlock">
-					<a
-						className="blockLink"
-						href="/events"
-						style={{
-							textDecoration: 'none',
-						}}
-					>
-						<Row>
-							<Col>
-								<img src={Lloyd} alt="Picture of stuff" className="personPic" />
-							</Col>
 
-							<Col>
-								<h3>Lloyd B. Minor</h3>
-								<hr></hr>
-								<p className="title">Dean of Stanford’s Medical School</p>
-								<p className="date">
-									February 12, 2019 | 6:00pm - 7:30pm | 125 Morrison Hall, UC
-									Berkeley
-								</p>
-								<p className="content">
-									How are medical schools adapting to today’s greatest
-									challenges? Join the Dean of Stanford University School of
-									Medicine, Lloyd B. Minor M.D., for a discussion on the ways in
-									which medical education is changing and responding to unique
-									social demands. Dean Minor will specifically discuss the
-									challenges in biomedical discovery and the vital importance of
-									diversity in science . . . <u>Click to see more</u>
-								</p>
-							</Col>
-						</Row>
-					</a>
-				</Container>
-			</section>
-
-			<section className="block">
-				<Container className="personBlock">
-					<a
-						className="blockLink"
-						href="/events"
-						style={{
-							textDecoration: 'none',
-						}}
-					>
-						<Row>
-							<Col>
-								<img src={Lloyd} alt="Picture of stuff" className="personPic" />
-							</Col>
-
-							<Col>
-								<h3>Lloyd B. Minor</h3>
-								<hr></hr>
-								<p className="title">Dean of Stanford’s Medical School</p>
-								<p className="date">
-									February 12, 2019 | 6:00pm - 7:30pm | 125 Morrison Hall, UC
-									Berkeley
-								</p>
-								<p className="content">
-									How are medical schools adapting to today’s greatest
-									challenges? Join the Dean of Stanford University School of
-									Medicine, Lloyd B. Minor M.D., for a discussion on the ways in
-									which medical education is changing and responding to unique
-									social demands. Dean Minor will specifically discuss the
-									challenges in biomedical discovery and the vital importance of
-									diversity in science . . . <u>Click to see more</u>
-								</p>
-							</Col>
-						</Row>
-					</a>
-				</Container>
-			</section>
+			<h4>Current/Upcoming Events</h4>
+			{currentEventData.map((speaker) => (
+				<Row xs={1}>
+					<EventCard
+						name={speaker[0]}
+						title={speaker[1]}
+						date={speaker[2]}
+						content={speaker[3]}
+						image={speaker[4]}
+						link={speaker[5]}
+						google={speaker[6]}
+						extra={speaker[7]}
+						className="event-card"
+					/>
+				</Row>
+			))}
 
 			<Footer></Footer>
 		</div>
