2f165b9a190d8491d41d413bd3c80facb7e2e424
Aditya Bhawal <aditya.bhawal@gmail.com>
2020-08-23 14:45:35 -0700

    Mobile Foot pics pls (progress)

--- src/components/A-WebElements/Footer.js
+++ src/components/A-WebElements/Footer.js
@@ -11,74 +11,141 @@ import Col from 'react-bootstrap/Col';
 
 const Footer = (props) => {
 	return (
-		<Container fluid className="footer">
-			<Col
-				xs={5}
-				className="left-col"
-				style={{ borderRight: '2px solid #FFFFFF' }}
-			>
-				<img src={tbfLogoPath} alt="tbf-logo" className="tbf-logo" />
-				<div className="contact-info">
-					<p>The Berkeley Forum at the University of California</p>
-					<p>432 Eshleman Hall, Berkeley, CA 94720-4520</p>
-					<p>
-						General Inquiries
-						<br />
-						info@forum.berkeley.edu
-					</p>
-				</div>
-			</Col>
-			<Col xs={7} className="right-col">
-				<Row className="footer-links">
-					<Col xs={3} className="footer-col">
+		<div className="footer">
+			<Container fluid className="footer-desktop">
+				<Col
+					xs={5}
+					className="left-col"
+					style={{ borderRight: '2px solid #FFFFFF' }}
+				>
+					<img src={tbfLogoPath} alt="tbf-logo" className="tbf-logo" />
+					<div className="contact-info">
+						<p>The Berkeley Forum at the University of California</p>
+						<p>432 Eshleman Hall, Berkeley, CA 94720-4520</p>
+						<p>
+							General Inquiries
+							<br />
+							info@forum.berkeley.edu
+						</p>
+					</div>
+				</Col>
+				<Col xs={7} className="right-col">
+					<Row className="footer-links">
+						<Col xs={3} className="footer-col">
+							<Link to="/" className="link-tag">
+								<p>Home</p>
+							</Link>
+							<Link to="/about" className="link-tag">
+								<p>About Us</p>
+							</Link>
+							<Link to="/conversations" className="link-tag">
+								<p>Conversations</p>
+							</Link>
+						</Col>
+
+						<Col xs={3} className="footer-col">
+							<Link to="/events" className="link-tag">
+								<p>Events</p>
+							</Link>
+							<Link to="/events" className="link-tag">
+								<p>Calendar</p>
+							</Link>
+							<Link to="/past-events" className="link-tag">
+								<p>Past Events</p>
+							</Link>
+						</Col>
+						<Col xs={3} className="footer-col">
+							<Link to="/team" className="link-tag">
+								<p>Meet Our Team</p>
+							</Link>
+							<Link to="/advisory" className="link-tag">
+								<p>Advisory Committee</p>
+							</Link>
+							<Link to="/join" className="link-tag">
+								<p>Join Us</p>
+							</Link>
+						</Col>
+
+						<Col xs={3} className="footer-col">
+							<Link to="/contact" className="link-tag">
+								<p>Contact Us</p>
+							</Link>
+							<Link to="" className="link-tag">
+								<p>Privacy Policy</p>
+							</Link>
+						</Col>
+					</Row>
+					<Row className="logo">
+						<img src={wdbLogoPath} alt="wdb-logo" className="wdb-logo" />
+					</Row>
+				</Col>
+			</Container>
+			<Container fluid className="footer-mobile">
+				<Row>
+					<Col>
+						{' '}
 						<Link to="/" className="link-tag">
 							<p>Home</p>
 						</Link>
-                                                <Link to="/about" className="link-tag">
+						<Link to="/about" className="link-tag">
 							<p>About Us</p>
 						</Link>
 						<Link to="/conversations" className="link-tag">
 							<p>Conversations</p>
 						</Link>
 					</Col>
-
-					<Col xs={3} className="footer-col">
-                                                <Link to="/events" className="link-tag">
+					<Col>
+						<Link to="/events" className="link-tag">
 							<p>Events</p>
 						</Link>
 						<Link to="/events" className="link-tag">
 							<p>Calendar</p>
 						</Link>
-                                                <Link to="/past-events" className="link-tag">
+						<Link to="/past-events" className="link-tag">
 							<p>Past Events</p>
 						</Link>
 					</Col>
-					<Col xs={3} className="footer-col">
-                                                <Link to="/team" className="link-tag">
-							<p>Meet Our Team</p>
-						</Link>
-												<Link to="/advisory" className="link-tag">
-							<p>Advisory Committee</p>
-						</Link>
-						<Link to="/join" className="link-tag">
-							<p>Join Us</p>
-						</Link>
-					</Col>
-
-					<Col xs={3} className="footer-col">
-                                                <Link to="/contact" className="link-tag">
+				</Row>
+				<Row>
+					<Col>
+						<Link to="/contact" className="link-tag">
 							<p>Contact Us</p>
 						</Link>
 						<Link to="" className="link-tag">
 							<p>Privacy Policy</p>
 						</Link>
 					</Col>
+					<Col>
+						<Link to="/team" className="link-tag">
+							<p>Meet Our Team</p>
+						</Link>
+						<Link to="/advisory" className="link-tag">
+							<p>Advisory Committee</p>
+						</Link>
+						<Link to="/join" className="link-tag">
+							<p>Join Us</p>
+						</Link>
+					</Col>
 				</Row>
-				<Row className="logo">
-					<img src={wdbLogoPath} alt="wdb-logo" className="wdb-logo" />
+				<Row>
+					<Col>
+						<img src={tbfLogoPath} alt="tbf-logo" className="tbf-logo" />
+						<img src={wdbLogoPath} alt="wdb-logo" className="wdb-logo" />
+					</Col>
+					<Col>
+						<div className="contact-info">
+							<p>The Berkeley Forum at the University of California</p>
+							<p>432 Eshleman Hall, Berkeley, CA 94720-4520</p>
+							<p>
+								General Inquiries
+								<br />
+								info@forum.berkeley.edu
+							</p>
+						</div>
+					</Col>
 				</Row>
-			</Col>
-		</Container>
+			</Container>
+		</div>
 	);
 };
 
